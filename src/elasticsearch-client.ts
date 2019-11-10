33c8d4a0aef64a9654f57540194b6c2ec42b6032
Navneet Lal Gupta <navneetlalg@gmail.com>
2019-11-10 18:24:29 +0530

    added retry method to create index in elasticsearch

--- src/elasticsearch-client.ts
+++ src/elasticsearch-client.ts
@@ -1,8 +1,16 @@
 import elasticsearch from 'elasticsearch';
+import promiseRetry from 'promise-retry';
+import { OperationOptions } from 'retry';
 
 class ElasticSearchClient {
   public client: elasticsearch.Client;
   public indexName: string;
+
+  private retryOperationOptions: OperationOptions = {
+    minTimeout: 2000,
+    retries: 10,
+    maxTimeout: 4000
+  }
 
   constructor(indexName: string) {
     this.indexName = indexName;
@@ -12,28 +20,39 @@ class ElasticSearchClient {
       apiVersion: '7.4'
     });
 
-    this.initializeMapping().catch(err => console.log(err));
+    this.initializeIndex();
+  }
+
+  private initializeIndex() {
+    promiseRetry(this.retryOperationOptions, (retry) => {
+      return this.indexExist().then((exist: boolean) => {
+        if (exist) {
+          return this.initializeMapping()
+        } else {
+          return this.client.indices.create({
+            index: this.indexName
+          }).then(() => this.initializeMapping())
+        }
+      }).catch(err => retry(err));
+    })
+
   }
 
   private initializeMapping() {
-    return this.indexExist().then((exist: boolean) => {
-      if (exist) {
-        return this.client.indices.putMapping({
-          index: this.indexName,
-          type: "document",
-          body: {
-            properties: {
-              title: { type: "text" },
-              content: { type: "text" },
-              suggest: {
-                type: "completion",
-                analyzer: "simple",
-                search_analyzer: "simple"
-              }
-            }
+    return this.client.indices.putMapping({
+      index: this.indexName,
+      type: "document",
+      body: {
+        properties: {
+          title: { type: "text" },
+          content: { type: "text" },
+          suggest: {
+            type: "completion",
+            analyzer: "simple",
+            search_analyzer: "simple"
           }
-        });
-      } else throw new Error('Oops..! Cannot find the index');
+        }
+      }
     });
   }
 
